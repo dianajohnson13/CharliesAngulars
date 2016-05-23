910c44cb11154c1e51dbf021aee95a9cebb7972e
Diana <dianajohnson13@gmail.com>
2016-05-23 14:37:18 -0700

    (doc) begin building weather module

--- client/app/weather/weather.js
+++ client/app/weather/weather.js
@@ -1,1 +1,24 @@
 // build module, controller and factory for weather api
+angular.module('ParksAndEx.weather', []) 
+  //should not be on MainPage (curr mainPage is in homescreen.js) 
+      // - need to put a module in app.js (name ParksAndEx)
+
+.controller('WeatherController', function($scope, WeatherFactory) {
+  $scope.forcast;
+
+  $scope.getWeather = function() {
+    WeatherController.requestWeather();
+  }
+
+})
+
+.factory('WeatherFactory', function() {
+
+  var requestWeather = function() {
+    // will facilitate request for weather..
+  }
+
+  return {
+
+  }
+});
