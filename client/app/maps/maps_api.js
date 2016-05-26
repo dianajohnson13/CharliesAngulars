0f9d241894d3e055e8dfdf91ca949d81646204cc
metamonkey8 <chen.liu.michael@gmail.com>
2016-05-26 12:42:14 -0700

    got rid of whitespaces in map_api.js

--- client/app/maps/maps_api.js
+++ client/app/maps/maps_api.js
@@ -1,13 +1,7 @@
 angular.module('parksAndEx.googleApi', [])
-
 .controller('googleApiController', function ($scope) {
-	
 	$scope.locations = [];
-	
-	
-	
 	handleAddress("San Francisco");
-	
 	function callbackFn(results, status) {
 		if (status === google.maps.places.PlacesServiceStatus.OK) {
 		console.log(results);
@@ -17,40 +11,29 @@ angular.module('parksAndEx.googleApi', [])
 			createMarker(results[i]);
 		  }
 		}
-	}
-
+	};
 	function createMarker(place) {
 		var placeLoc = place.geometry.location;
 		var marker = new google.maps.Marker({
 		  map: map,
 		  position: place.geometry.location
 		});
-
 		google.maps.event.addListener(marker, 'click', function() {
 		  infowindow.setContent(place.name);
 		  infowindow.open(map, this);
 		});
-	}
-	  
+	}; 
 	function handleAddress(input) {
 		var formattedInput = input.split(' ').join('%20');
 		httpGetAsync('https://maps.googleapis.com/maps/api/geocode/json?address=' + formattedInput + '&key=AIzaSyAvP71A4zQ3bBjri75-1y6AaLP3s-JfNO0', handleLocation);
-		
-	}
-	
+	};
 	function handleLocation(input) {
-		
 		var inputJSON = JSON.parse(input);
-		 
-	  
-		
 		var location = {lat: inputJSON.results[0].geometry.location.lat, lng: inputJSON.results[0].geometry.location.lng};
-
 		map = new google.maps.Map(document.getElementById('map'), {
 		  center: location,
 		  zoom: 12
 		});
-
 		infowindow = new google.maps.InfoWindow();
 		var service = new google.maps.places.PlacesService(map);
 		service.nearbySearch({
@@ -58,9 +41,7 @@ angular.module('parksAndEx.googleApi', [])
 		  radius: 2500,
 		  type: ['park']
 		}, callbackFn);
-	  
-	}
-
+	};
 	function httpGetAsync(theUrl, callback)
 	{
 		var xmlHttp = new XMLHttpRequest();
@@ -70,7 +51,7 @@ angular.module('parksAndEx.googleApi', [])
 		}
 		xmlHttp.open("GET", theUrl, true); // true for asynchronous 
 		xmlHttp.send();
-	}
+	};
 });
 
 // This example requires the Places library. Include the libraries=places
