12f93ce66180bbf1ac07bcdcff59793a021a2d47
Bo <boyao2005@gmail.com>
2016-05-27 10:11:42 -0700

    (feat)add ng toggle switch for tabs

---
+++ client/app/toggleNavbar/toggleNavbar.js
@@ -0,0 +1,9 @@
+angular.module('parksAndEx.toggleNavbar', [])
+
+.controller('toggleNavbarController', function($scope) {
+  $scope.tab1 = false;
+  $scope.tab2 = false;
+  $scope.tab3 = false;
+  $scope.tab4 = false;
+  $scope.tab5 = false;
+});
