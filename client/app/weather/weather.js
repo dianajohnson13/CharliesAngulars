59185a0b13f9a02c628a4c7d046baf8da945c16b
Diana <dianajohnson13@gmail.com>
2016-05-27 07:59:48 -0700

    add todays weather to weatherController scope

--- client/app/weather/weather.js
+++ client/app/weather/weather.js
@@ -1,19 +1,26 @@
 angular.module('parksAndEx.weather', []) 
 
 .controller('weatherController', function($scope, weatherFactory) {
-  $scope.forecast;
+  $scope.sevenDayForecast;
+  $scope.todaysWeather;
 
   $scope.generateWeather = function(lat, lon) {
     weatherFactory.generateWeather(lat, lon);
   }
 
+  $scope.$on('todaysWeather', function(event, args) {
+    $scope.todaysWeather = args;
+    console.log(args)
+    //args currently contains just the current temp
+  });
+
   $scope.generateWeather(37.77, -122.419); //hardcoded...
 })
 
-.factory('weatherFactory', function() {
+.factory('weatherFactory', function($rootScope) {
   var apiKey = '4efbcc23711c1a198e5242ff3bd69d7a';
-  var urlTodayStart = 'http://api.openweathermap.org/data/2.5/weather?'
-  var urlSevenDayStart = 'http://api.openweathermap.org/data/2.5/forecast/daily?'
+  var urlTodayStart = 'http://api.openweathermap.org/data/2.5/weather?';
+  var urlSevenDayStart = 'http://api.openweathermap.org/data/2.5/forecast/daily?';
   
   var latitude;
   var longitude;
@@ -22,18 +29,25 @@ angular.module('parksAndEx.weather', [])
     latitude = lat;
     longitude = lon;
 
-    httpGetAsync(setUrl(urlTodayStart), doSomethingWithTodaysForecast);
-    httpGetAsync(setUrl(urlSevenDayStart), doSomethingWithSevenDayForecast);
+    httpGetAsync(setUrl(urlTodayStart), handleTodaysForecast);
+    //httpGetAsync(setUrl(urlSevenDayStart), handleSevenDayForecast);
   }
 
-  function doSomethingWithTodaysForecast(resp){
-    // dont worry. I'll change these function names
+  function handleTodaysForecast(resp){
     console.log('today', resp);
+    $rootScope.$broadcast('todaysWeather', { 
+      currTemp: Math.round(kelvinToFahrenheit(resp.main.temp)),
+      highTemp: Math.round(kelvinToFahrenheit(resp.main.temp_max)),
+      lowTemp: Math.round(kelvinToFahrenheit(resp.main.temp_min)),
+      description: resp.weather[0].description,
+      icon: resp.weather[0].icon
+    });
   }
 
-  function doSomethingWithSevenDayForecast(resp){
-    // dont worry. I'll change these function names
+  function handleSevenDayForecast(resp){
     console.log('sevenday', resp);
+    //for each:  day, format date, high temp, low temp, description, icon
+        // put all of these in a forcast obj
   }
 
   function httpGetAsync(url, callback) {
@@ -41,7 +55,7 @@ angular.module('parksAndEx.weather', [])
     
     xmlHttp.onreadystatechange = function() { 
       if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
-        callback(xmlHttp.responseText);
+        callback(JSON.parse(xmlHttp.responseText));
       }
     }
     xmlHttp.open("GET", url, true);
@@ -52,13 +66,13 @@ angular.module('parksAndEx.weather', [])
     return urlStart + 'lat=' + latitude + '&lon=' + longitude + '&APPID=' + apiKey;
   }
 
+  function kelvinToFahrenheit(temp) {
+    return (temp * 9/5) - 459.67;
+  }
+
   // function formatDate(timeStamp) {
   //   var date = new Date(timeStamp * 1000);
   //   var day = date.getDate();
-  // }
-
-  // function convertTemp() {
-  //
   // }
 
   return {
